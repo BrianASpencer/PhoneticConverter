d510964f102a6769d530d77eaef739dab0c6ef07
Brian Spencer <46761632+BrianASpencer@users.noreply.github.com>
2020-08-25 23:50:18 -0400

    Update phonetic.py
    Made side of program reflect screen dimensions of user.

--- src/phonetic.py
+++ src/phonetic.py
@@ -1,76 +1,87 @@
 import tkinter as tk
-
+import pyautogui
 from tkinter import Text
 
 
-root = tk.Tk()
-root.geometry("800x480")
+d = {
+    "a": "Alpha",
+    "b": "Bravo",
+    "c": 'Charlie',
+    "d": "Delta",
+    "e": "Echo",
+    "f": "Foxtrot",
+    "g": "Golf",
+    "h": "Hotel",
+    "i": "India",
+    "j": "Juliett",
+    "k": "Kilo",
+    "l": "Lima",
+    "m": "Mike",
+    "n": "November",
+    "o": "Oscar",
+    "p": "Papa",
+    "q": "Quebec",
+    "r": "Romeo",
+    "s": "Sierra",
+    "t": "Tango",
+    "u": "Uniform",
+    'v': "Victor",
+    'w': "Whiskey",
+    "x": "X-ray",
+    "y": "Yankee",
+    "z": "Zulu"
+}
 
-textBox = tk.Text(root, height=10)
-textBox.pack()
+root = tk.Tk()
+# dimensions
+width, height = pyautogui.size()
+widthStr = str(width//3)
+heightStr = str(height//12)
+resolution = widthStr + "x" + heightStr
+root.geometry(resolution)
+
+textBox = tk.Text(root, height=5, width=width//30)
+textBox.grid(row=1, column=0)
+
 
 def clearText():
-    textBox.delete(1.0,"end")
+    textBox.delete(1.0, "end")
+
 
 def setText(s):
     textBox.insert(1.0, s)
 
+
 def readText(string):
-    d = {
-        "a" : "Alpha" ,                                                                                                                                                       
-        "b" : "Bravo" ,                                                                                                                                                       
-        "c" : 'Charlie' ,                                                                                                                                                     
-        "d" : "Delta" ,                                                                                                                                                       
-        "e" : "Echo" ,                                                                                                                                                        
-        "f" : "Foxtrot" ,                                                                                                                                                     
-        "g" : "Golf" ,                                                                                                                                                        
-        "h" : "Hotel" ,                                                                                                                                                       
-        "i" : "India" ,                                                                                                                                                       
-        "j" : "Juliett" ,                                                                                                                                                     
-        "k" : "Kilo" ,                                                                                                                                                        
-        "l" : "Lima" ,                                                                                                                                                        
-        "m" : "Mike" ,                                                                                                                                                        
-        "n" : "November" ,                                                                                                                                                    
-        "o" : "Oscar" ,                                                                                                                                                       
-        "p" : "Papa" ,                                                                                                                                                        
-        "q" : "Quebec" ,                                                                                                                                                      
-        "r" : "Romeo" ,                                                                                                                                                       
-        "s" : "Sierra" ,                                                                                                                                                      
-        "t" : "Tango" ,                                                                                                                                                       
-        "u" : "Uniform" ,                                                                                                                                                     
-        'v' : "Victor" ,                                                                                                                                                      
-        'w' : "Whiskey" ,                                                                                                                                                     
-        "x" : "X-ray" ,                                                                                                                                                       
-        "y" : "Yankee" ,                                                                                                                                                      
-        "z" : "Zulu"
-    }
-    
+
     string = string.lower()
 
     out = ""
-    
+
     for i in string:
         if i in d:
             out = out + d[i] + " "
     setText(out)
+
 
 def processText():
     result = textBox.get(1.0, tk.END+"-1c")
     clearText()
     readText(result)
 
-btnConv = tk.Button(root, 
-                   height=1, 
-                   width=10, 
-                   text="Convert", 
-                   command=lambda:processText())
-btnConv.pack()
 
-btnClear = tk.Button(root, 
-                   height=1, 
-                   width=10, 
-                   text="Clear", 
-                   command=lambda:clearText())
-btnClear.pack()
+btnConv = tk.Button(root,
+                    height=1,
+                    width=10,
+                    text="Convert",
+                    command=lambda: processText())
+btnConv.grid(row=1, column=1)
+
+btnClear = tk.Button(root,
+                     height=1,
+                     width=10,
+                     text="Clear",
+                     command=lambda: clearText())
+btnClear.grid(row=1, column=2)
 
 root.mainloop()
