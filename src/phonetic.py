c956a5832463985a845b950e11ca710bd54f2ecc
Brian Spencer <46761632+BrianASpencer@users.noreply.github.com>
2020-08-31 09:41:49 -0400

    Update phonetic.py
    Now has partially bold output to make it more obvious what the original word was.

--- src/phonetic.py
+++ src/phonetic.py
@@ -42,7 +42,7 @@ label.grid(row=1, column=0, sticky="W")
 # dimensions
 root.geometry("790x115")
 
-textBox = tk.Text(root, height=5, width=78)
+textBox = tk.Text(root, font="Helvetica 12", height=5, width=78)
 textBox.grid(row=2, column=0)
 
 
@@ -50,8 +50,16 @@ def clearText():
     textBox.delete(1.0, "end")
 
 
-def setText(s):
-    textBox.insert(1.0, s)
+def setText(string):
+    textBox.tag_configure("bold", font="Helvetica 14 bold")
+    for i in range(len(string)-1, -1, -1):
+        if i == 0:
+            textBox.insert(1.0, string[i], "bold")
+        else:
+            if string[i-1] == " ":
+                textBox.insert(1.0, string[i], "bold")
+            else:
+                textBox.insert(1.0, string[i])
 
 
 def readText(string):
@@ -85,6 +93,7 @@ btnClear = tk.Button(root,
                      text="Clear",
                      command=lambda: clearText())
 btnClear.grid(row=2, column=1)
+
 
 
 root.mainloop()
